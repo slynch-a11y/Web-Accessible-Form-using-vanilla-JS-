344f13b2f9bca25a9b720bff7746127cdb679fe8
Sarah Lynch <sarah@alum.rpi.edu>
2018-11-30 12:01:33 -0500

    add thank you when form filled out correctly

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict'
+
 let errors = []
 
 function validateForm() {
@@ -6,13 +7,11 @@ function validateForm() {
   let lastName1 = document.forms.myForm.lastName1.value
   let email1 = document.forms.myForm.email1.value
   let phone1 = document.forms.myForm.phone1.value
-
-  let errorDiv = document.getElementById('errors')
-
-  if (document.forms.myForm.submit.value === 'Submit'){
-    errorDiv.style.display = 'block'
-    let errorMessage = document.getElementById('errorMessage')
-    errorMessage.focus()
+  let errors = []
+  var child = document.getElementById('myList')
+  if (child){
+    child.parentNode.removeChild(child)
+  }
 
     if (name1 === '') {
       let errorMessageLink = document.createElement('a')
@@ -38,30 +37,51 @@ function validateForm() {
       errorMessageLink.innerHTML = 'Phone must be filled out'
       errors.push(errorMessageLink)
     }
-  var list = document.createElement('ul')
-  list.setAttribute('id', 'myList')
-  var myList = errorMessage.appendChild(list)
-  for (let i = 0; i < errors.length; i++){
-  var li = document.createElement('li')
-  myList.appendChild(li)
-  li.appendChild(errors[i])
+
+
+  if (document.forms.myForm.submit.value === 'Submit' && errors.length){
+    let errorDiv = document.getElementById('errors')
+    errorDiv.style.display = 'block'
+    let errorMessage = document.getElementById('errorMessage')
+    errorMessage.focus()
+
+    var list = document.createElement('ul')
+    list.setAttribute('id', 'myList')
+    var myList = errorMessage.appendChild(list)
+    for (let i = 0; i < errors.length; i++){
+    var li = document.createElement('li')
+    myList.appendChild(li)
+    li.appendChild(errors[i])
+    }
+
   }
-  return false
+else if (document.forms.myForm.submit.value === 'Submit' && errors.length<1){
+  let thankYouDiv = document.getElementById('thankYou')
+  thankYouDiv.style.display = 'block'
+  let errorDiv = document.getElementById('errors')
+  errorDiv.style.display = 'none'
 }
+return false
 }
 
 function clearForm(){
   //set the error display style back to 'none'
   let errorDiv = document.getElementById('errors')
   errorDiv.style.display = 'none'
+  //set the thankYou div style back to 'none'
+  let thankYouDiv = document.getElementById('thankYou')
+  thankYouDiv.style.display = 'none'
   //reset the errors array to empty array
   errors = []
   //remove ul and li elements from their parent element
   var child = document.getElementById('myList')
-  child.parentNode.removeChild(child)
+  if (child){
+    child.parentNode.removeChild(child)
+  }
   //reset the values of input fields to empty strings
   for (let i = 0; i < document.forms.myForm.length - 2; i++){
     document.forms.myForm[i].value = ''
   }
   return false
 }
+
